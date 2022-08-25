b68ccebb91a89f4129142764aaad2b82f9965d58
asd15-pass <70442362+asd15-pass@users.noreply.github.com>
2022-08-25 13:09:04 +0900

    prepare

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <h1>React課題</h1>
+          <nav>
+            <ul className="Flex">
+              <li className="ListStyle">
+                <a href="/general4.tsx">総合課題４</a>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/general4.tsx" element={<General4 />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
-
+// react-router-dom入れたとこ
 export default App;
